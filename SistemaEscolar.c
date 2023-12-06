0dc4aae23f6b05c1f11af0a2a9b28027945eafc1
Nathy Brito <20222tadssaj0025@ifba.edu.br>
2023-12-06 14:49:58 -0300

    busca de elementos

--- SistemaEscolar.c
+++ SistemaEscolar.c
@@ -121,3 +121,54 @@ void insereProfessores(cadastroProfessores *listaProfessores, char nome, cadastr
     }
     
 }
+
+
+
+//funcao para buscar um aluno na lista
+int buscarAluno(cadastroAlunos cadastro, char nome[], char matricula[]){
+    for (int i = 0; i < cadastro.tamanhoListaAlunos; i++)
+    {
+        if (strcmp(cadastro.alunos[i].nome, nome == 0 || strcmp(cadastro.alunos[i].matricula == 0)))
+        {
+            //retorna o indice do aluno
+            return i; 
+        }
+    }
+    return printf("aluno não encontrado");
+}
+
+//funcao para buscar um curso na lista
+int buscarCurso(cadastroCursos cadastro2, char nomeCurso[], double cargaHoraria){
+    for (int i = 0; i < cadastro2.tamanhoListaCursos; i++)
+    {
+        if (strcmp(cadastro2.cursos[i].nomeCurso == 0 || strcmp(cadastro2.cursos[i].cargaHoraria, cargaHoraria == 0)))
+        {
+            return i;
+        }
+        
+    }
+    return printf("curso não encontrado");
+}
+
+//funcao para buscar turmas na lista
+int buscarTurmas(cadastroTurmas cadastro3, char identificador, cadastroAlunos alunosMatriculados, cadastroProfessores professoresResponsavies){
+    for (int i = 0; i < cadastro3.tamanhoListaTurmas; i++)
+    {
+        if (strcmp(cadastro3.turmas[i].identificador, identificador == 0 || strcmp(cadastro3.turmas[i].alunosMatriculados, alunosMatriculados == 0))){
+            return i;
+        }
+    }
+    return printf("turma não encontrada");
+}
+
+//funcao para buscar professores na lista
+    int buscarProfessor(cadastroProfessores cadastro4, char nome[],char disciplinasLecionadas, cadastroTurmas turmasMinistradas){
+        for (int i = 0; i < cadastro4.tamanhoListaProfessores; i++)
+        {
+            if (strcmp(cadastro4.professores[i].nome, nome == 0 || strcmp(cadastro4.professores[i].disciplinasLecionadas, disciplinasLecionadas == 0 || cadastro4.professores[i].turmasMinistradas, turmasMinistradas == 0)))
+            {
+                return i;
+            }
+        }
+        return printf("professor não encontrado");
+    }
